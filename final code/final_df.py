4b69865019e65741a3865b7f1ac9044a49e5e737
Slou <slou412@gmail.com>
2017-01-25 18:07:16 +0100

    toutes les images sont prises en compte, changement des features de position

--- final code/final_df.py
+++ final code/final_df.py
@@ -11,7 +11,7 @@ import glob
 import os
 import pandas as pd
 from quality_features import blurry
-from spatial_features import dist_rule_thirds, get_face_center,face_ratio
+from spatial_features import dist_rule_thirds, get_face_center,face_ratio, eye_position
 
 #popo
 pathname = "/Users/paulinenicolas/Documents/M2_Data_Science/ML_From_Theory_To_Practice/Project_ML/challenge_training_input_file_predict_the_aesthetic_score_of_a_portrait_by_combining_photo_analysis_and_facial_attributes_analysis/pictures_train/*.jpg"
@@ -19,13 +19,18 @@ pathname = "/Users/paulinenicolas/Documents/M2_Data_Science/ML_From_Theory_To_Pr
 pathname = '/Users/domitillecoulomb/Documents/DATA_SCIENCE/MachineLearning/Projet/pictures_test/*.jpg'
 path_data = '/Users/domitillecoulomb/Documents/DATA_SCIENCE/MachineLearning/Projet/facial_features_train.csv'
 
-data = pd.read_csv(path_data, nrows=20)
+#slou
+pathname = '/home/slou/Documents/M2/semestre1/ML_project/pictures_train/*.jpg'
+path_data = '/home/slou/Documents/M2/semestre1/ML_project/facial_features_train.csv'
+
+data = pd.read_csv(path_data)
 
 #adding our features to a global dataframe with the picture id
 df = pd.DataFrame(columns=('ID', 'blur'
                             ,'d_p1','d_p2','d_p3','d_p4'
                             ,'d_l1','d_l2','d_l3', 'd_l4'
-                            , 'face_ratio'))
+                            , 'face_ratio'
+                            , 'left_eye_lvl', 'right_eye_level'))
 
 i = 0
 for img in glob.glob(pathname):
@@ -35,16 +40,19 @@ for img in glob.glob(pathname):
     idx = int(os.path.splitext(os.path.basename(img))[0])
     
     #get saptial features
-    a,b = get_face_center(image.shape[1], image.shape[0]
-                        , data['x0'].ix[idx-1], data['width'].ix[idx-1]
-                        , data['y0'].ix[idx-1], data['height'].ix[idx-1])
+    a,b = get_face_center(data['x0'].ix[idx-1], data['width'].ix[idx-1], data['y0'].ix[idx-1], data['height'].ix[idx-1])
                         
-    drt = dist_rule_thirds(image.shape[1], image.shape[0], a,b)
+    drt = dist_rule_thirds(a,b)
+    
+    eyes_level = eye_position(data['left_eye_y'].ix[idx-1], data['right_eye_y'].ix[idx-1])
+    
     
     #Filling the df line by line
     df.loc[i] = [os.path.splitext(os.path.basename(img))[0]
                 , blurry(image)
                 , drt[0], drt[1],drt[2],drt[3],drt[4],drt[5],drt[6],drt[7]
-                , face_ratio(data['width'].ix[idx-1], data['height'].ix[idx-1])]
+                , face_ratio(data['width'].ix[idx-1], data['height'].ix[idx-1])
+                , eyes_level[0], eyes_level[1]]
     i+=1
 
+print(df.iloc[0])
--- final code/spatial_features.py
+++ final code/spatial_features.py
@@ -6,50 +6,57 @@ Created on Tue Jan 24 11:37:06 2017
 @author: paulinenicolas
 """
 
-import cv2
-
 #### Location of the face ####
 
 #### Regle des trois traits ####
 
+from math import hypot
 from numpy.linalg import norm
 
-def get_face_center(width_im, height_im, x_0, width_f, y_0, height_f):
+def get_face_center(x_0, width_f, y_0, height_f):
     'x_0, width_f, y_0, height_f in percentage'
-    'width_im, height_im in number of pixels' 
     
-    x_c = int((x_0 + width_f/2) * width_im)
-    y_c = int((y_0 + height_f/2) * height_im)
+    x_c = int(x_0 + width_f/2)
+    y_c = int(y_0 + height_f/2)
     
     return (x_c, y_c) 
 
-def dist_rule_thirds(width_im, height_im, x_c, y_c): 
+def dist_rule_thirds(x_c, y_c): 
     """
-    x_c, y_c position of the center of the face in number of pixels
-    width_im, height_im in number of pixels
+    x_c, y_c position of the center of the face in number %
     """
+    top_third = 1/3
+    bot_third = 1/3
+    
     
     '''distance between the face center & the 4 points (rule of thirds)'''
-
-    hw = width_im * height_im # normalization
     
-    dist_1 = norm(((height_im/3 - y_c), (width_im/3-x_c)))/hw
-    dist_2 = norm(((height_im/3 - y_c),(2*width_im/3-x_c)))/hw
-    dist_3 = norm(((2*height_im/3 - y_c),(2*width_im/3-x_c)))/hw
-    dist_4 = norm(((2*height_im/3 - y_c),(width_im/3-x_c)))/hw
+    dist_1 = hypot((top_third - y_c),(top_third - x_c))
+    dist_2 = hypot((top_third - y_c),(bot_third - x_c))
+    dist_3 = hypot((bot_third - y_c),(bot_third - x_c))
+    dist_4 = hypot((bot_third - y_c),(top_third - x_c))
     
     '''distance between the face center & the 4 lines (rule of thirds)'''
 
-    dist_l1 = norm(((height_im/3-x_c)))/hw #vertical 1
-    dist_l2 = norm(((2*height_im/3)-x_c))/hw #vertical 2
-    dist_l3 = norm(((width_im/3-y_c)))/hw # horizontal 1
-    dist_l4 = norm(((2*width_im/3-y_c)))/hw # horizontal 2
+    dist_l1 = norm(top_third - x_c) #vertical 1
+    dist_l2 = norm(bot_third - x_c) #vertical 2
+    dist_l3 = norm(top_third - y_c) # horizontal 1
+    dist_l4 = norm(bot_third - y_c) # horizontal 2
     
     return (dist_1, dist_2, dist_3, dist_4, dist_l1,dist_l2,dist_l3,dist_l4)
 
 def face_ratio(width_f, height_f):
     """
-    Ratio of face to image: Approx number of pixels of the face to that in the background
+    Ratio of face to image
     """
     ratio = width_f * height_f
     return ratio
+
+def eye_position(y_left_eye, y_right_eye):
+    """
+    Distance between eyes and the top third of the picture
+    """
+    top_third = 1/3
+    dist_eye1 = norm(top_third-y_left_eye)
+    dist_eye2 = norm(top_third-y_right_eye)
+    return (dist_eye1, dist_eye2)
