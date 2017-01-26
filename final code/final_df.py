b64669f717fd3a3a6239d75bacee79ff7074474d
Slou <slou412@gmail.com>
2017-01-26 10:53:46 +0100

    add svr

--- final code/final_df.py
+++ final code/final_df.py
@@ -10,8 +10,15 @@ import cv2
 import glob
 import os
 import pandas as pd
+import numpy as np
+from sklearn.svm import SVR
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import mean_squared_error
+
+
 from quality_features import blurry
 from spatial_features import dist_rule_thirds, get_face_center,face_ratio, eye_position
+
 
 #popo
 pathname = "/Users/paulinenicolas/Documents/M2_Data_Science/ML_From_Theory_To_Practice/Project_ML/challenge_training_input_file_predict_the_aesthetic_score_of_a_portrait_by_combining_photo_analysis_and_facial_attributes_analysis/pictures_train/*.jpg"
@@ -21,9 +28,11 @@ path_data = '/Users/domitillecoulomb/Documents/DATA_SCIENCE/MachineLearning/Proj
 
 #slou
 pathname = '/home/slou/Documents/M2/semestre1/ML_project/pictures_train/*.jpg'
+pathresult = '/home/slou/Documents/M2/semestre1/ML_project/output_train.csv'
 path_data = '/home/slou/Documents/M2/semestre1/ML_project/facial_features_train.csv'
 
 data = pd.read_csv(path_data)
+result = pd.read_csv(pathresult, sep =";")
 
 #adding our features to a global dataframe with the picture id
 df = pd.DataFrame(columns=('ID', 'blur'
@@ -46,13 +55,32 @@ for img in glob.glob(pathname):
     
     eyes_level = eye_position(data['left_eye_y'].ix[idx-1], data['right_eye_y'].ix[idx-1])
     
+    brightness = brightness(image)
+    
     
     #Filling the df line by line
     df.loc[i] = [os.path.splitext(os.path.basename(img))[0]
                 , blurry(image)
                 , drt[0], drt[1],drt[2],drt[3],drt[4],drt[5],drt[6],drt[7]
                 , face_ratio(data['width'].ix[idx-1], data['height'].ix[idx-1])
-                , eyes_level[0], eyes_level[1]]
+                , eyes_level[0], eyes_level[1]
+                , ]
     i+=1
 
-print(df.iloc[0])
+#Split data
+#X_train, X_test, y_train, y_test = train_test_split(x_selection_position, y_position_impact, train_size=0.8, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(df, result['TARGET'], train_size=0.8, random_state=0)
+print("Nb d'échantillons d'apprentissage :  {}".format(X_train.shape[0]))
+print("Nb d'échantillons de validation :    {}".format(X_test.shape[0]))
+
+#Train the model 
+svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
+svr_rbf.fit(X_train, y_train)
+
+#Prediction
+y_pred = svr_rbf.predict(X_test)
+
+# accuracy : mean square error
+print("Mean Squared error: {}", mean_squared_error(y_test, y_pred))
+print(y_pred[:10])
+print(np.std(y_pred))
