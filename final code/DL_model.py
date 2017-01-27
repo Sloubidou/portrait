e1e260cb66e1f1965a4053edbb5a8f04e50d8541
Estelle Aflalo <estelleaflalo@eduroam-0-82.enst.fr>
2017-01-27 15:46:43 +0100

    DL works

--- final code/DL_model.py
+++ final code/DL_model.py
@@ -1,17 +1,11 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jan 26 21:46:40 2017
-
-@author: estelleaflalo
-"""
-
-import cv2
+mport cv2
 import math
 import numpy as np
+import glob
 import pandas as pd
 from keras.applications.vgg16 import VGG16
 from imagenet_utils import decode_predictions, preprocess_input
+from keras.optimizers import SGD
 pathname="/Users/estelleaflalo/Desktop/M2_Data_Science/First_Period/Machine_Learning_from_Theory_to_Practice/Project/challenge_fichier_dentrees_dentrainement_predire_le_score_esthetique_dun_portrait/pictures_train/*.jpg"
 pathresult = "/Users/estelleaflalo/Desktop/M2_Data_Science/First_Period/Machine_Learning_from_Theory_to_Practice/Project/challenge_fichier_de_sortie_dentrainement_predire_le_score_esthetique_dun_portrait.csv"
 path_data = "/Users/estelleaflalo/Desktop/M2_Data_Science/First_Period/Machine_Learning_from_Theory_to_Practice/Project/challenge_fichier_dentrees_dentrainement_predire_le_score_esthetique_dun_portrait/facial_features_train.csv"
@@ -22,54 +16,32 @@ df_score = pd.read_csv(pathresult, sep = ';')
 
 im_test="/Users/estelleaflalo/Desktop/M2_Data_Science/First_Period/Machine_Learning_from_Theory_to_Practice/Project/challenge_fichier_dentrees_dentrainement_predire_le_score_esthetique_dun_portrait/pictures_train/25.jpg"
 
-def preprocess_input(x, dim_ordering='default'):
-    """Preprocesses a tensor encoding a batch of images.
-    # Arguments
-        x: input Numpy tensor, 4D.
-        dim_ordering: data format of the image tensor.
-    # Returns
-        Preprocessed tensor.
-    """
-    if dim_ordering == 'default':
-        dim_ordering = K.image_dim_ordering()
-    assert dim_ordering in {'tf', 'th'}
 
-    if dim_ordering == 'th':
-        # 'RGB'->'BGR'
-        x = x[:, ::-1, :, :]
-        # Zero-center by mean pixel
-        x[:, 0, :, :] -= 103.939
-        x[:, 1, :, :] -= 116.779
-        x[:, 2, :, :] -= 123.68
-    else:
-        # 'RGB'->'BGR'
-        x = x[:, :, :, ::-1]
-        # Zero-center by mean pixel
-        x[:, :, :, 0] -= 103.939
-        x[:, :, :, 1] -= 116.779
-        x[:, :, :, 2] -= 123.68
-    return x
-width=[]
-height=[]
-for img in glob.glob(pathname):
-    width.append(cv2.imread(img).shape[1])
-    height.append(cv2.imread(img).shape[0])
-
-max_width=max(width) #6381
-max_height=max(height) #6016
-
-for img in glob.glob(pathname):
-    
-    #im=cv2.imread(img)
-    #im=cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    #resize=np.zeros((max_height,max_width))
-    #resize[:im.shape[0],:im.shape[1]]=im
-    #print resize, resize.shape
-
+im=cv2.imread(im_test)
+im = cv2.resize(im, (224, 224)).astype(np.float32)
+im[:,:,0] -= 103.939
+im[:,:,1] -= 116.779
+im[:,:,2] -= 123.68
+#im = im.transpose((2,0,1))
+im = np.expand_dims(im, axis=0)
 model = VGG16(weights="imagenet")
+sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(optimizer=sgd, loss='categorical_crossentropy')
+out_0 = model.predict(im)
 
-im=cv2.imread(im_test)[:224,:224,:]
-x = np.expand_dims(im, axis=0)
-preds = model.predict(x)
+out_old=out_0
+for img in glob.glob(pathname):
+    im=cv2.imread(im_test)
+    im = cv2.resize(im, (224, 224)).astype(np.float32)
+    im[:,:,0] -= 103.939
+    im[:,:,1] -= 116.779
+    im[:,:,2] -= 123.68
 
-
+#im = im.transpose((2,0,1))
+    im = np.expand_dims(im, axis=0)
+    model = VGG16(weights="imagenet")
+    sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(optimizer=sgd, loss='categorical_crossentropy')
+    out = model.predict(im) 
+    feat=np.concatenate((out, out_old), axis=0)
+    out_old=feat
