ad4ba85bb466e3f6ca3048b1f1b2b3ba3a7b1d54
Pauline Nicolas <pauline.nicolas1@gmail.com>
2017-01-28 12:05:30 +0100

    change C value cross val

--- final code/cross_val.py
+++ final code/cross_val.py
@@ -8,9 +8,10 @@ Created on Tue Jan 17 12:56:14 2017
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
+import pdb
 
 
-pathname1 = "/Users/paulinenicolas/Documents/M2_Data_Science/ML_From_Theory_To_Practice/Project_ML/portrait/result.csv"
+pathname1 = "/Users/paulinenicolas/portrait/df_modif.csv"
 pathname2 = "/Users/paulinenicolas/Documents/M2_Data_Science/ML_From_Theory_To_Practice/Project_ML/challenge_output_data_training_file_predict_the_aesthetic_score_of_a_portrait_by_combining_photo_analysis_and_facial_attributes_analysis.csv"
 
 #Features Data
@@ -37,7 +38,7 @@ class FeatureExtractorClf(object):
         #selecting only features that are interesting for the prdiction (i.e impact feature)
         XX = X_df.copy()
         XX = XX.set_index(['ID'])
-        XX = XX.drop(['Unnamed: 0', 'd_p1', 'd_p2', 'd_p3', 'd_p4', 'd_l1', 'd_l2', 'd_l3', 'd_l4'], axis=1)
+        XX = XX.drop(['Unnamed: 0'], axis=1)
         #XX = XX.select_dtypes(include=['float64', 'int'])
         XX = np.array(XX)
         # mean = 0 ; standard deviation = 1.0
@@ -51,7 +52,8 @@ class FeatureExtractorClf(object):
 
 
         #del yy['TARGET']
-        yy = np.array(yy)
+        yy = yy.values.tolist()
+        yy = [item for sublist in yy for item in sublist]
 
         return XX, yy
 
@@ -69,8 +71,7 @@ class Classifier(BaseEstimator):
     def __init__(self, C):
         self.n_components = 10
         self.C = C
-        self.clf = Pipeline([('pca', PCA(n_components=self.n_components)), 
-                             ('clf', SVC(C = self.C, probability = True))]) 
+        self.clf = SVC(C = self.C, probability = False) 
 
     def fit(self, X, y):
         self.clf.fit(X, y)
@@ -109,8 +110,9 @@ def train_test_model_clf(X_df, y_df, skf_is, FeatureExtractor, Classifier):
     clf.fit(X_train_array_clf, y_train_array_clf)
     
     # Test          
-    y_proba_clf = clf.predict_proba(X_test_array_clf)                        
-    y_pred_clf = labels[np.argmax(y_proba_clf, axis=1)]                      
+    #y_proba_clf = clf.predict_proba(X_test_array_clf)                        
+    #y_pred_clf = labels[np.argmax(y_proba_clf, axis=1)] 
+    y_pred_clf = clf.predict(X_test_array_clf)                     
     accuracy = spearman_error(y_test_array_clf, y_pred_clf)      
     print(y_pred_clf[:10], y_test_array_clf[:10])                                   
     return accuracy
@@ -128,7 +130,7 @@ def spearman_error(y_true, y_pred):
 
     
 #Cross Validation in order to find  the value of C which predict the best   
-C = [0.001, 0.01, 0.1, 1]
+C = [10, 100, 1000]
  
  
 accuracies = []
